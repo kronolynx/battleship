6f20e48dc07055d83ff0da0bc1035fe62d677c92
kronolynx <kronolynx@gmail.com>
2016-01-09 12:59:05 +0000

    Add user views and user options to navigate menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "user/registrations"}
   root 'welcome#index'
+  get '/users', to: "users#index"
+  get '/user/:id', to: 'users#show', as: 'user'
 end
