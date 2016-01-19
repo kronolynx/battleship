9fdfdd885c6f9ac42c02044fc7b0a748d9b1ef89
kronolynx <kronolynx@gmail.com>
2016-01-19 10:24:31 +0000

    Add custom logger for successful logins

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -3,6 +3,9 @@ class User::SessionsController < Devise::SessionsController
     super
     current_user.online = "online"
     current_user.save
+    # log to file succesful logins 
+    MyLog.info("#{current_user.email} logged successfully from #{current_user.current_sign_in_ip}")
+    
   end
   
   def destroy
