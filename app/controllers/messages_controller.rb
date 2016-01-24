3765c95322247656791690b1845b783262a78177
kronolynx <kronolynx@gmail.com>
2016-01-24 18:30:44 +0100

    Complete chat implementation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,12 @@
 class MessagesController < ApplicationController
-  
+
   def create
     @battle = Battle.find(message_params[:battle_id])
     @message = @battle.messages.build(message_params)
     @message.user_id = current_user.id
     @message.save!
-    
-    PrivatePub.publish_to battle_path(@battle, "aler('test'")
+
+    #PrivatePub.publish_to battle_path(@battle, "aler('test'")
     @path = battle_path(@battle)
   end
   def message_params
