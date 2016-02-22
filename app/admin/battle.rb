12c902feeeb7d9205a81593db020cb8bb4ba644b
kronolynx <kronolynx@gmail.com>
2016-02-22 13:33:25 +0100

    Add admin action for importing battles

--- app/admin/battle.rb
+++ app/admin/battle.rb
@@ -17,6 +17,43 @@ ActiveAdmin.register Battle do
     column :active_player
     actions
   end
+  csv do
+    column :id
+    column :player_id
+    column :enemy_id
+    column :player_board
+    column :enemy_board
+    column :active_player
+  end
+
+
+  collection_action :import, :method=>:get do
+    MyLog.info("Inside import get")
+    # render the view
+  end
+  collection_action :import, :method=>:post do
+    MyLog.info("Inside import post")
+    # read the csv and for each line create a new battle
+  end
+
+  #TODO fix the links
+  action_item do
+    link_to "Import battle", import_admin_battles_path #'app/views/admin/battles/import.html.haml'
+  end
+
+  # not sure yet if this will be needed
+  #controller do
+  #  def import
+   #   if request.post?
+   #     MyLog.info("Inside import post")
+   #     redirect admin_battles_path
+    #  else
+    #    render partial: 'import'
+    #    MyLog.info("Inside import get")
+   #   end
+   # end
+  #end
   config.filters = false
-  
+
+
 end
